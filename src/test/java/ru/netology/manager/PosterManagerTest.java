9193537c92ccf0d96aaa96f2ecbdfcd1c7d0efaf
Kasparidi <kasparidi@gmail.com>
2020-10-24 14:48:50 +0300

    fixed types

--- src/test/java/ru/netology/manager/PosterManagerTest.java
+++ src/test/java/ru/netology/manager/PosterManagerTest.java
@@ -9,11 +9,11 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PosterManagerTest {
 
     PosterManager manager = new PosterManager();
-    PosterItem first = new PosterItem(1, 2, "first", 1, 1);
-    PosterItem second = new PosterItem(2, 2, "second", 2, 2);
-    PosterItem third = new PosterItem(3, 3, "third", 3, 3);
-    PosterItem forth = new PosterItem(4, 4, "forth", 4, 4);
-    PosterItem fifth = new PosterItem(5, 5, "fifth", 5, 5);
+    PosterItem first = new PosterItem(1, 2, "first", "comedy", true);
+    PosterItem second = new PosterItem(2, 2, "second", "drama", true);
+    PosterItem third = new PosterItem(3, 3, "third", "thriller", true);
+    PosterItem forth = new PosterItem(4, 4, "forth", "fiction", true);
+    PosterItem fifth = new PosterItem(5, 5, "fifth", "fantasy", true);
 
     @BeforeEach
     public void SetUp(){
@@ -26,11 +26,11 @@ public class PosterManagerTest {
 
     @Test
     void shouldAddTen() {
-        PosterItem sixth = new PosterItem(6, 6, "sixth", 6, 6);
-        PosterItem seventh = new PosterItem(7, 7, "seventh", 7, 7);
-        PosterItem eighth = new PosterItem(8, 8, "eighth", 8, 8);
-        PosterItem ninth = new PosterItem(9, 9, "ninth", 9, 9);
-        PosterItem tenth = new PosterItem(10, 10, "tenth", 10, 10);
+        PosterItem sixth = new PosterItem(6, 6, "sixth", "comedy", true);
+        PosterItem seventh = new PosterItem(7, 7, "seventh", "drama", true);
+        PosterItem eighth = new PosterItem(8, 8, "eighth", "thriller", true);
+        PosterItem ninth = new PosterItem(9, 9, "ninth", "fiction", true);
+        PosterItem tenth = new PosterItem(10, 10, "tenth", "fantasy", true);
         manager.add(sixth);
         manager.add(seventh);
         manager.add(eighth);
@@ -50,12 +50,12 @@ public class PosterManagerTest {
 
     @Test
     void shouldNotAddMoreThanTen() {
-        PosterItem sixth = new PosterItem(6, 6, "sixth", 6, 6);
-        PosterItem seventh = new PosterItem(7, 7, "seventh", 7, 7);
-        PosterItem eighth = new PosterItem(8, 8, "eighth", 8, 8);
-        PosterItem ninth = new PosterItem(9, 9, "ninth", 9, 9);
-        PosterItem tenth = new PosterItem(10, 10, "tenth", 10, 10);
-        PosterItem eleventh = new PosterItem(11, 11, "eleventh", 11, 11);
+        PosterItem sixth = new PosterItem(6, 6, "sixth", "comedy", true);
+        PosterItem seventh = new PosterItem(7, 7, "seventh", "drama", true);
+        PosterItem eighth = new PosterItem(8, 8, "eighth", "thriller", true);
+        PosterItem ninth = new PosterItem(9, 9, "ninth", "fiction", true);
+        PosterItem tenth = new PosterItem(10, 10, "tenth", "fantasy", true);
+        PosterItem eleventh = new PosterItem(11, 11, "eleventh", "comedy", true);
         manager.add(sixth);
         manager.add(seventh);
         manager.add(eighth);
