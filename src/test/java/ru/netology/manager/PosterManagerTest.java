7606569d0bf4a3086aa7e15254df3ed46bc64d86
Kasparidi <kasparidi@gmail.com>
2020-10-29 07:19:16 +0300

    second rework

--- src/test/java/ru/netology/manager/PosterManagerTest.java
+++ src/test/java/ru/netology/manager/PosterManagerTest.java
@@ -14,7 +14,12 @@ public class PosterManagerTest {
     PosterItem third = new PosterItem(3, 3, "third", "thriller", true);
     PosterItem forth = new PosterItem(4, 4, "forth", "fiction", true);
     PosterItem fifth = new PosterItem(5, 5, "fifth", "fantasy", true);
-
+    PosterItem sixth = new PosterItem(6, 6, "sixth", "comedy", true);
+    PosterItem seventh = new PosterItem(7, 7, "seventh", "drama", true);
+    PosterItem eighth = new PosterItem(8, 8, "eighth", "thriller", true);
+    PosterItem ninth = new PosterItem(9, 9, "ninth", "fiction", true);
+    PosterItem tenth = new PosterItem(10, 10, "tenth", "fantasy", true);
+    PosterItem eleventh = new PosterItem(11, 11, "eleventh", "comedy", true);
 
     @BeforeEach
     public void SetUp(){
@@ -49,22 +54,16 @@ public class PosterManagerTest {
         assertArrayEquals(expected, actual);
     }
 
-//    @Test
-//    void shouldNotAddMoreThanTen() {
-//        PosterItem sixth = new PosterItem(6, 6, "sixth", "comedy", true);
-//        PosterItem seventh = new PosterItem(7, 7, "seventh", "drama", true);
-//        PosterItem eighth = new PosterItem(8, 8, "eighth", "thriller", true);
-//        PosterItem ninth = new PosterItem(9, 9, "ninth", "fiction", true);
-//        PosterItem tenth = new PosterItem(10, 10, "tenth", "fantasy", true);
-//        PosterItem eleventh = new PosterItem(11, 11, "eleventh", "comedy", true);
-//        manager.add(sixth);
-//        manager.add(seventh);
-//        manager.add(eighth);
-//        manager.add(ninth);
-//        manager.add(tenth);
-//        manager.add(eleventh);
-//        PosterItem[] actual = manager.getAll();
-//        PosterItem[] expected = new PosterItem[] {eleventh, tenth, ninth, eighth, seventh, sixth, fifth, forth, third, second, null};
-//        assertArrayEquals(expected, actual);
-//    }
+    @Test
+    void shouldNotAddMoreThanTen() {
+        manager.add(sixth);
+        manager.add(seventh);
+        manager.add(eighth);
+        manager.add(ninth);
+        manager.add(tenth);
+        manager.add(eleventh);
+        PosterItem[] actual = manager.getAll();
+        PosterItem[] expected = new PosterItem[] {eleventh, tenth, ninth, eighth, seventh, sixth, fifth, forth, third, second};
+        assertArrayEquals(expected, actual);
+    }
 }
